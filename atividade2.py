b58705a390623007d2693ef4b9ab0390c2e655e2
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-16 18:10:47 -0300

    :tada: feat: atividade 2

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+#Calcule o somatório dos números de 1 a 100 e imprima o resultado.
+soma   = 0
+numero = 0
+for numero in range(1,101):
+    soma = soma + numero
+    print("Atual posição:",numero)
+  
+print("A soma de todos os números de 1 a 100 foi: ", soma)
+
+    
